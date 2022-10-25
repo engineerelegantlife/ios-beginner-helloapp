02722eaf5f3c343960e0015c37ad21fdd0f54613
Tomo Shimizu <tomoshimizu@TomoMac.local>
2022-10-26 08:15:27 +0900

    first commit

---
+++ HelloApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HelloApp
+//
+//  Created by Tomo Shimizu on 2022/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
